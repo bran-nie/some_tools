6dd19a7caa59543101e05694dcea93b61b4bc5d1
PengCheng Nie <pengcheng1995@gmail.com>
2019-09-06 17:03:35 +0800

    尝试写一个log的类

---
+++ log/index.js
@@ -0,0 +1,33 @@
+const successStyle = 'color: green;'
+const warnStyle = 'color: orange;'
+const errorStyle = 'color: red;'
+const infoStyle = 'color: black;'
+
+
+class Log {
+    constructor() {
+
+    }
+
+    success(...v) {
+        v = v.join(' ')
+        console.log(`%c${v}`, successStyle)
+    }
+
+    warn(...v) {
+        v = v.join(' ')
+        console.log(`%c${v}`, warnStyle)
+    }
+
+    error(...v) {
+        v = v.join(' ')
+        console.log(`%c${v}`, errorStyle)
+    }
+
+    info(...v) {
+        v = v.join(' ')
+        console.log(`%c${v}`, infoStyle)
+    }
+}
+const log = new Log()
+export default log
